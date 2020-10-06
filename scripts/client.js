8f47dc0108d59d4cf3d8b52c118355a710f8b665
Myron <dubtwee@gmail.com>
2020-10-06 11:26:57 -0500

    object review

--- scripts/client.js
+++ scripts/client.js
@@ -2,10 +2,63 @@
 // Object Literals
 // ------------------------------
 
+const fruit = {};
+
+fruit.name = 'Orange';
+
+console.log(fruit.name);
+
+const topHat = {
+  type: 'Top',
+  color: 'Royal Blue',
+  size: 12,
+
+  // restrictions on Naming
+  'bil-style': 'curved',
+};
+
+console.log(topHat.type);
+console.log(topHat['type']);
+console.log(topHat.bil_style);
+
+const arrayOfKeys = Object.keys(topHat);
+console.log(arrayOfKeys);
+
+for (let keyValue of arrayOfKeys) {
+  console.log(topHat[keyValue]);
+}
+
 //
 // Object Parts
 // ------------------------------
 
+// {}
+// properties - inside of object
+// { key: 'value' }
+// . - get values (object.type)
+// [] - (object['type'])
+
 //
 // Object Constructors
 // ------------------------------
+
+// blueprinting
+function Hat(type, color, size) {
+  this.type = type;
+  this.color = color;
+  this.size = size;
+  this.isWearing = true;
+}
+
+// instance
+// blueprint
+// constructor
+// new (keyword)
+
+console.log(topHat);
+const ballCap = new Hat('ball cap', null, 13);
+ballCap.isWearing = false;
+console.log(ballCap);
+// instance of Hat
+const newsyHat = new Hat('newsy', 'teal', 11);
+console.log(newsyHat);
